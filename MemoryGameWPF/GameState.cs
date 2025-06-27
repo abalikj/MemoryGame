635f6508bd069ee1725fe74135d467ef3ac9d456
abalikj <aidabalik@gmail.com>
2025-06-27 16:17:32 +0200

    Added new class

---
+++ MemoryGameWPF/GameState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryGameWPF
+{
+    [Serializable]
+    public class GameState
+    {
+        public List<Card> Cards { get; set; }
+        public int TotalMatches { get; set; }
+        public int Tries { get; set; }
+        public int Seconds { get; set; }
+        public bool IsPlayerTurn { get; set; }
+        public string Difficulty { get; set; }
+
+        // Чуваме индексите на картите што ботот ги памети, по патеката (сликата)
+        public Dictionary<string, List<int>> BotMemoryIndices { get; set; }
+    }
+}
