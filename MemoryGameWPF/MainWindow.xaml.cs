1a3fbd7ec39f0c94cbd2fefe38f9bff2db21349d
tearusevska <tearusevska@yahoo.com>
2025-06-29 12:06:16 +0200

    Update MainWindow.xaml.cs

--- MemoryGameWPF/MainWindow.xaml.cs
+++ MemoryGameWPF/MainWindow.xaml.cs
@@ -319,7 +319,7 @@ namespace MemoryGameWPF
 
             // sega panelot
             EndGamePanel.Visibility = Visibility.Visible;
-            EndGameMessage.Text = "What would you like to do next?";
+            EndGameMessage.Text = "What do you like to do next?";
             EndGameStats.Text = $"Level: {difficulty}\nTime: {seconds} seconds\nMoves: {tries}";
 
 
